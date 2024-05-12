c84f449a40c9486e42a9b6e8992f9064c0854962
Hassan Abedi <hassan.abedi.t@gmail.com>
2024-05-12 21:49:17 +0200

    Reorganize the code and directories
    Created the recommended Terraform files and renamed src to modules.

---
+++ modules/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+}
