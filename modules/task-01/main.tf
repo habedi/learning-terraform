9f484a7f3751242ba38bdb519856600832e310ac
Hassan Abedi <hassan.abedi.t@gmail.com>
2024-08-17 14:52:21 +0200

    Update the files
    * Updated the minimum Python version to 3.10
    * Updated the EditorConfig file

--- modules/task-01/main.tf
+++ modules/task-01/main.tf
@@ -30,16 +30,16 @@ resource "aws_security_group" "allow_ssh" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
